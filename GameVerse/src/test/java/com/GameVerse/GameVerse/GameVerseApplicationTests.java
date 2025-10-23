26412472e1a3138a1c9f404c7618ee65d5e98ff0
Alandis Patterson <alandisp@email.sc.edu>
2025-10-23 12:02:33 -0400

    adding bases technologies to repo

---
+++ GameVerse/src/test/java/com/GameVerse/GameVerse/GameVerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GameVerse.GameVerse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameVerseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
