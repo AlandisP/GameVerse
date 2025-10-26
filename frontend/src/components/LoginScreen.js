eadd9af24464a91fc9ea3bdcf6b7aeea91ae7739
Joshua Cook <jc157@email.sc.edu>
2025-10-26 16:20:11 -0400

    testing merge conflicts

--- frontend/src/components/LoginScreen.js
+++ frontend/src/components/LoginScreen.js
@@ -1,55 +1,47 @@
-import './styles.css';
-import { useNavigate } from 'react-router-dom';
-import React, { useState } from 'react';
+import "./styles.css";
+import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
 function LoginScreen() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
+  const HandleLogin = async () => {
+    /* Testing the merge conflict resolution. */
+  };
 
-    const HandleLogin = async() => {
-        try {
-            if(!username || !password) {
-                setError('Please fill in the fields');
-                return;
-            }
-        }
-    }
+  const HandleClick = () => {
+    alert("I Clicked");
+  };
 
-    const HandleClick = () =>{
-        alert("I Clicked");
-    }
-
-    return (
-        <div className="LoginScreen">
-            <h1>Please Login!</h1>
-            <div className="Username">
-                <h3>Username</h3>
-                <input 
-                className="InputBox" 
-                type='text'
-                id= 'userinput'
-                placeholder='Username'
-                onChange={(e) => setUsername(e.target.value)}
-                />
-            </div>
-            <div className="Password">
-                <h3>Password</h3>
-                <input 
-                className="InputBox" 
-                type='text'
-                id='passinput'
-                placeholder='Password'
-                onChange={(e) => setPassword(e.target.value)}
-                />
-            </div>
-            <button className = "LoginButton" onClick={HandleClick}>
-                Login
-            </button>
-
-        </div>
-    )
-
+  return (
+    <div className="LoginScreen">
+      <h1>Please Login!</h1>
+      <div className="Username">
+        <h3>Username</h3>
+        <input
+          className="InputBox"
+          type="text"
+          id="userinput"
+          placeholder="Username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div className="Password">
+        <h3>Password</h3>
+        <input
+          className="InputBox"
+          type="text"
+          id="passinput"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button className="LoginButton" onClick={HandleClick}>
+        Login
+      </button>
+    </div>
+  );
 }
 
 export default LoginScreen;
