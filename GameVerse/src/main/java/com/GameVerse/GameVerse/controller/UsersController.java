5c637a3f982de630c5f59ca15391402893c7bb24
Alandis Patterson <alandisp@email.sc.edu>
2025-11-25 20:49:46 -0500

    added an API method that returns a list of users based on certain text. Also started creating Party Finder functionality.

--- GameVerse/src/main/java/com/GameVerse/GameVerse/controller/UsersController.java
+++ GameVerse/src/main/java/com/GameVerse/GameVerse/controller/UsersController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.GameVerse.GameVerse.model.User;
@@ -65,4 +66,12 @@ public class UsersController {
         
         return "Test data inserted! Total users: " + repository.count();
     }
+
+    @GetMapping("/matches")
+    public List<User> getUsernameMatches(@RequestParam String text) {
+
+        return repository.findByUsernameContainingIgnoreCase(text);
+
+    }
+
 }
